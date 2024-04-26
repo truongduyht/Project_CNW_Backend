0e2a53e8a72c52433c9adc64f01f4539120664dd
B2014734 <duyb2014734@student.ctu.edu.vn>
2024-04-26 14:34:27 +0700

    commit

--- src/models/index.js
+++ src/models/index.js
@@ -3,7 +3,7 @@ import Reader from "./DocGiaModel";
 import Publish from "./NXBModel";
 import Book from "./SachModel";
 import Cart from "./GioSachModel";
-import Order from "./DocGiaModel";
+import Order from "./DonSachModel";
 import OrderDetail from "./TheoDoiDonSachModel";
 
 export default { Staff, Reader, Publish, Book, Cart, Order, OrderDetail };
